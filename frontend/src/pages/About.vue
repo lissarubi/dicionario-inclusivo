515c9888c86e8139481ac61286ef92d67169b930
Lissa Ferreira <lissadeveloper@gmail.com>
2022-07-06 15:31:44 -0400

    [feat] minibio na página de sobre

--- frontend/src/pages/About.vue
+++ frontend/src/pages/About.vue
@@ -13,11 +13,10 @@
     <h2>Quem criou?</h2>
     <p>
       O dicionário inclusivo foi criado pela
-      <a href="https://twitter.com/lissatransborda" target="_blank"
-        >Lissa Ferreira</a
-      >, produtora de conteúdo, Community Manager na
-      <a href="https://feministech.github.io" target="_blank">Feministech</a>,
-      uma comunidade para pessoas que se identificam no feminino e não bináries
+      <a href="https://twitter.com/lissatransborda" target="_blank">Lissa Ferreira</a>. Produtora de conteúdo, Community
+      Manager da comunidade <a href="https://feministech.github.io" target="_blank">Feministech</a>, Cofundadora da
+      comunidade <a href="https://kotlinautas.dev/">Kotlinautas</a>,
+      moderadora da mentoria de DevRel <a href="https://abacatinhos.dev/">Abacatinhos.dev</a> e cozinheira amadora.
     </p>
   </section>
 
@@ -26,11 +25,8 @@
     <p>
       Para saber mais sobre o funcionamento do dicionário inclusivo, o projeto
       está disponível no
-      <a
-        href="https://github.com/lissatransborda/dicionario-inclusivo"
-        target="_blank"
-        >Github</a
-      >, com todo o código fonte aberto.
+      <a href="https://github.com/lissatransborda/dicionario-inclusivo" target="_blank">Github</a>, com todo o código
+      fonte aberto.
     </p>
   </section>
 </template>
