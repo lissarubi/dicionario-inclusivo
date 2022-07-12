c2ae4f1916df1b5da76ba71066ae8783bff47255
Lissa Ferreira <lissadeveloper@gmail.com>
2022-07-11 20:01:09 -0400

    [feat] adicionando varíavel de ambiente para definir se o servidor está rodando em modo de desenvolvimento ou não

--- backend/README.md
+++ backend/README.md
@@ -10,15 +10,16 @@ Backend do projeto, que recebe o texto original e corrige as palavras, usando o 
 http POST /translate content="eu amo todos daqui"
 ```
 
-## Variáveis de ambiente
-
-- `CLIENT_URL`: URL do cliente (frontend), caso essa variável não seja especificada, qualquer host será aceito.
-
 E com isso será retornada a resposta:
 
 ```
 eu amo todes aqui
 ```
+
+## Variáveis de ambiente
+
+- `DEVELOPMENT`: Varíavel de ambiente que define se o servidor está rodando em modo de desenvolvimento ou não. Por padrão, é `true`.
+- `CLIENT_URL`: URL do cliente (frontend), caso essa variável não seja especificada, qualquer host será aceito.
 
 ## Iniciar aplicação
 
--- backend/src/main/kotlin/database/initDB.kt
+++ backend/src/main/kotlin/database/initDB.kt
@@ -5,16 +5,16 @@ import org.jetbrains.exposed.sql.SchemaUtils
 import org.jetbrains.exposed.sql.transactions.transaction
 import schemas.NewWords
 
+val development = System.getenv("DEVELOPMENT") ?: true
 val user = System.getenv("DATABASE_USER") ?: "root"
 val password = System.getenv("DATABASE_PASSWORD") ?: "root"
 val server = System.getenv("DATABASE_SERVER") ?: "localhost:5432"
 
 fun initDB() {
-  // use H2 during development
-  //    Database.connect("jdbc:h2:mem:regular;DB_CLOSE_DELAY=-1;", "org.h2.Driver")
-
-  // use postgres in production
-  Database.connect("jdbc:postgresql://$server/dicionarioinclusivo", driver = "org.postgresql.Driver", user, password)
-
+  if (development as Boolean){
+    Database.connect("jdbc:h2:mem:regular;DB_CLOSE_DELAY=-1;", "org.h2.Driver")
+  }else{
+    Database.connect("jdbc:postgresql://$server/dicionarioinclusivo", driver = "org.postgresql.Driver", user, password)
+  }
   transaction { SchemaUtils.create(NewWords) }
 }
