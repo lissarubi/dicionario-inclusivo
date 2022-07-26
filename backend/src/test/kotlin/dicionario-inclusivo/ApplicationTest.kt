b47d07010d31792fa19708a3f4ec9c2d228dbf44
Lissa Ferreira <lissadeveloper@gmail.com>
2022-07-25 20:07:37 -0400

    [fix] arrumando erro de importação no arquivo de testes

--- backend/src/test/kotlin/dicionario-inclusivo/ApplicationTest.kt
+++ backend/src/test/kotlin/dicionario-inclusivo/ApplicationTest.kt
@@ -4,16 +4,6 @@ import dicionarioinclusivo.plugins.*
 import io.ktor.client.request.*
 import io.ktor.client.statement.*
 import io.ktor.http.*
-import io.ktor.serialization.kotlinx.json.*
-import io.ktor.server.application.*
-import io.ktor.server.locations.*
-import io.ktor.server.plugins.compression.*
-import io.ktor.server.plugins.contentnegotiation.*
-import io.ktor.server.plugins.cors.routing.*
-import io.ktor.server.plugins.statuspages.*
-import io.ktor.server.request.*
-import io.ktor.server.response.*
-import io.ktor.server.routing.*
 import io.ktor.server.testing.*
 import kotlin.test.*
 
